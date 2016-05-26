f3a73f4f4545dfcb69dac9396950eea2569c0abf
aditya <aditya999123@gmail.com>
2016-05-26 16:42:17 +0530

    json with volley is implemented

--- app/src/main/java/app/startups/nitrr/ecell/ecellapp/ContactUs/ContactUs.java
+++ app/src/main/java/app/startups/nitrr/ecell/ecellapp/ContactUs/ContactUs.java
@@ -7,10 +7,21 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -31,6 +42,8 @@ public class ContactUs extends Activity {
     private RecyclerView.Adapter Radapter;
     private ArrayList<datavar> datals;
     Bitmap myBitmap;
+    int flag = 0;
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -45,8 +58,8 @@ public class ContactUs extends Activity {
 
         datals = new ArrayList<>();
 
-        new AsyncTaskParseJson().execute();
-        //datals.add(new datavar(3, "Aditya", "Tech", "7587485272", "aditya999123@gmail.com"));
+        //new AsyncTaskParseJson().execute();
+//        datals.add(new datavar(3, "Aditya", "Tech", "7587485272", "aditya999123@gmail.com"));
 
 
         recycler1 = (RecyclerView) findViewById(R.id.recycler1);
@@ -54,72 +67,63 @@ public class ContactUs extends Activity {
 
         recycler1.setLayoutManager(lmanager);
         Radapter = new adapter(getApplicationContext(), datals);
-        recycler1.setAdapter(Radapter);
+        datals.add(new datavar(3, "Aditya", "Tech", "7587485272", "aditya999123@gmail.com"));
 
-         }
+        String url = "http://adityaagr.tk/ContactJson3.json";
+        //final String[] strjson = {""};
+// Request a string response
 
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
 
-    public class AsyncTaskParseJson extends AsyncTask<String, String, String> {
+                        //           System.out.println(response);
+                        try {
+                            //             text1.setText("b");
+                            JSONObject jsonobj = new JSONObject(response);
 
-
-        String yourJsonStringUrl = "http://adityaagr.tk/ContactJson.json";
-
-        JSONArray dataJsonArr = null;
-
-        @Override
-        protected void onPreExecute() {
-        }
-
-        @Override
-        protected String doInBackground(String... arg0) {
-
-
-            try {
-
-                JsonParser jParser = new JsonParser();
-                 //datals.add(new datavar(3, "Aditya", "Tech", "7587485272", "aditya999123@gmail.com"));
-                String jsonstr = null;
-                //String jsonurl = "http://adityaagr.tk/ContactJson";
-                jsonstr = jParser.getJSONFromUrl();
-
-               // datals.add(new datavar(3, "Aditya3", "Tech", "7587485272", "aditya999123@gmail.com"));
-                //String jsonstr = "{\"Contacts\": [\n {\n\"name\": \"Ram2 Tamada\",\n\"email\": \"ravi@gmail.com\",\n\"designation\" : \"Tech\",\n\"phone\": \"+0000000000\"\n}\n]\n}";
-                if (jsonstr != null) {
-                    JSONObject jsonobj = new JSONObject(jsonstr);
-                 //   datals.add(new datavar(3, "Aditya4", "Tech", "7587485272", "aditya999123@gmail.com"));
+          //                  datals.add(new datavar(3, "Aditya4", "Tech", "7587485272", "aditya999123@gmail.com"));
 
 
-                   dataJsonArr = jsonobj.getJSONArray("Contacts");
-                 //   datals.add(new datavar(3, "Aditya", "Tech", "7587485272", "aditya999123@gmail.com"));
+                            JSONArray dataJsonArr = jsonobj.getJSONArray("Contacts");
 
-                    for (int i = 0; i < dataJsonArr.length(); i++) {
-                        JSONObject jsonObject = dataJsonArr.getJSONObject(i);
+            //                datals.add(new datavar(3, "Aditya", "Tech", "7587485272", "aditya999123@gmail.com"));
 
-                        String jsonname = jsonObject.optString("name").toString();
-                        String jsondsgn = jsonObject.optString("designation").toString();
-                        String jsonnumber = jsonObject.optString("phone").toString();
-                        String jsonemail = jsonObject.optString("email").toString();
+                            for (int i = 0; i < dataJsonArr.length(); i++) {
+                                JSONObject jsonObject = dataJsonArr.getJSONObject(i);
 
-
-                        //new ImageLoadTask("http://adityaagr.tk/"+jsonname+".jpg");
-
-                        datals.add(new datavar(i+1, jsonname, jsondsgn, jsonnumber, jsonemail));
+                                String jsonname = jsonObject.optString("name").toString();
+                                String jsondsgn = jsonObject.optString("designation").toString();
+                                String jsonnumber = jsonObject.optString("phone").toString();
+                                String jsonemail = jsonObject.optString("email").toString();
 
 
+                                //new ImageLoadTask("http://adityaagr.tk/"+jsonname+".jpg");
+
+                                datals.add(new datavar(i + 1, jsonname, jsondsgn, jsonnumber, jsonemail));
+
+
+                            }
+                            recycler1.setAdapter(Radapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
-                }
-                /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-            } catch (JSONException e) {
-                e.printStackTrace();
+                // Error handling
+                System.out.println("Something went wrong!");
+                error.printStackTrace();
             }
-            return null;
-        }
+        });
+
+// Add the request to the queue
+
+        Volley.newRequestQueue(this).add(stringRequest);
+
 
     }
-
-
-
 }
