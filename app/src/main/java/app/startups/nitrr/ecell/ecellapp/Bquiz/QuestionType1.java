324b712b32dc891564246de20d820031b6fadac7
iket0512 <iket.ag24@gmail.com>
2016-08-06 20:02:03 +0530

    bquiz xml

---
+++ app/src/main/java/app/startups/nitrr/ecell/ecellapp/Bquiz/QuestionType1.java
@@ -0,0 +1,13 @@
+package app.startups.nitrr.ecell.ecellapp.Bquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionType1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_type1);
+    }
+}
