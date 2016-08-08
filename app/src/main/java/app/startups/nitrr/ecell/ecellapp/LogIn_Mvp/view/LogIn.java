9948d432010c83bd45e4a6d6e64edc605e738d21
Meghal <m3gh4l@gmail.com>
2016-08-08 23:24:37 +0530

    Answer module initialization

--- app/src/main/java/app/startups/nitrr/ecell/ecellapp/LogIn_Mvp/view/LogIn.java
+++ app/src/main/java/app/startups/nitrr/ecell/ecellapp/LogIn_Mvp/view/LogIn.java
@@ -1,6 +1,7 @@
 package app.startups.nitrr.ecell.ecellapp.LogIn_Mvp.view;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +13,9 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.regex.Matcher;
@@ -24,8 +28,14 @@ import app.startups.nitrr.ecell.ecellapp.send_otp.view.SendOtp;
  * Created by Iket on 8/8/2016.
  */
 
-public class LogIn extends AppCompatActivity implements LogIn_View{
-    String refreshedToken="";
+public class LogIn extends AppCompatActivity implements LogIn_View {
+    String refreshedToken = "";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         int[] ar = {1, 2, 3, 4, 5, 6, 7, 8};
@@ -48,55 +58,55 @@ public class LogIn extends AppCompatActivity implements LogIn_View{
         spinner1.setOnItemSelectedListener(new MyOnItemSelectedListener());
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
+        assert toolbar != null;
         toolbar.setTitle("Log In");
 
         Button otp = (Button) findViewById(R.id.next_button);
         otp.setOnClickListener(
-                new View.OnClickListener()
-                {
-                    public void onClick(View view)
-                    {
+                new View.OnClickListener() {
+                    public void onClick(View view) {
                         EditText mEdit = (EditText) findViewById(R.id.name);
-                        String name=mEdit.getText().toString();
+                        String name = mEdit.getText().toString();
 
                         EditText mEdit1 = (EditText) findViewById(R.id.last_name);
-                        String lname=mEdit1.getText().toString();
+                        String lname = mEdit1.getText().toString();
 
                         EditText mEdit2 = (EditText) findViewById(R.id.email_id);
-                        String email=mEdit2.getText().toString();
+                        String email = mEdit2.getText().toString();
 
                         EditText mEdit3 = (EditText) findViewById(R.id.college);
-                        String college=mEdit3.getText().toString();
+                        String college = mEdit3.getText().toString();
 
-                        String branch=spinner1.getSelectedItem().toString();
+                        String branch = spinner1.getSelectedItem().toString();
 
                         refreshedToken = FirebaseInstanceId.getInstance().getToken();
-                        Log.d("ResponseOtp",branch);
+                        Log.d("ResponseOtp", branch);
 
-                        String sem=spinner.getSelectedItem().toString();
-                        if(emailInvalid(email))
-                        {
+                        String sem = spinner.getSelectedItem().toString();
+                        if (emailInvalid(email)) {
                             Toast.makeText(LogIn.this, "ENTER CORRECT EMAIL ID!",
                                     Toast.LENGTH_LONG).show();
-                        }
-                        else
+                        } else
 
 
                         {
-                            Intent i = new Intent(LogIn.this,SendOtp.class);
+                            Intent i = new Intent(LogIn.this, SendOtp.class);
                             i.putExtra("name", name);
                             i.putExtra("lname", lname);
                             i.putExtra("email", email);
                             i.putExtra("college", college);
                             i.putExtra("branch", branch);
                             i.putExtra("sem", sem);
-                            i.putExtra("token",refreshedToken);
+                            i.putExtra("token", refreshedToken);
                             Log.d("ResponseOtp", "" + name);
                             startActivity(i);
                         }
                     }
                 });
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -110,7 +120,47 @@ public class LogIn extends AppCompatActivity implements LogIn_View{
                         + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
         pattern = Pattern.compile(EMAIL_PATTERN);
         matcher = pattern.matcher(email);
-        boolean a=matcher.matches();
+        boolean a = matcher.matches();
         return !a;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "LogIn Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://app.startups.nitrr.ecell.ecellapp.LogIn_Mvp.view/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "LogIn Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://app.startups.nitrr.ecell.ecellapp.LogIn_Mvp.view/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
