a287bbdc94d7deb6337a884fee1efe21a956791b
iket0512 <iket.ag24@gmail.com>
2016-08-10 21:18:23 +0530

    post method done

--- app/src/main/java/app/startups/nitrr/ecell/ecellapp/send_otp/api/RequestInterface.java
+++ app/src/main/java/app/startups/nitrr/ecell/ecellapp/send_otp/api/RequestInterface.java
@@ -4,13 +4,15 @@ package app.startups.nitrr.ecell.ecellapp.send_otp.api;
 import app.startups.nitrr.ecell.ecellapp.helper.Urls;
 import app.startups.nitrr.ecell.ecellapp.send_otp.model.data.SendOtpData;
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 
 /**
  * Created by Iket on 8/6/2016.
  */
 public interface RequestInterface {
-    @GET(Urls.SEND_OTP)
-    Call<SendOtpData> getSuccess (@Query("name") String name,@Query("mobile") String mobile);
+    @FormUrlEncoded
+    @POST(Urls.SEND_OTP)
+    Call<SendOtpData> getSuccess (@Field("name") String name, @Field("mobile") String mobile);
 }
--- app/src/main/java/app/startups/nitrr/ecell/ecellapp/send_otp/api/VerifyInterface.java
+++ app/src/main/java/app/startups/nitrr/ecell/ecellapp/send_otp/api/VerifyInterface.java
@@ -3,13 +3,15 @@ package app.startups.nitrr.ecell.ecellapp.send_otp.api;
 import app.startups.nitrr.ecell.ecellapp.helper.Urls;
 import app.startups.nitrr.ecell.ecellapp.send_otp.model.data.VerifyOtpData;
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 
 /**
  * Created by Iket on 8/8/2016.
  */
 public interface VerifyInterface {
-    @GET(Urls.VERIFY_OTP)
-    Call<VerifyOtpData> getSuccess (@Query("name") String name, @Query("lname") String lname, @Query("email") String email, @Query("college")String college, @Query("sem")String sem, @Query("branch") String branch, @Query("mobile")String num, @Query("otp") String otp, @Query("fcm") String fcm);
+    @FormUrlEncoded
+    @POST(Urls.VERIFY_OTP)
+    Call<VerifyOtpData> getSuccess (@Field("name") String name, @Field("lname") String lname, @Field("email") String email, @Field("college") String college, @Field("sem") String sem, @Field("branch") String branch, @Field("mobile") String num, @Field("otp") String otp, @Field("fcm") String fcm);
 }
