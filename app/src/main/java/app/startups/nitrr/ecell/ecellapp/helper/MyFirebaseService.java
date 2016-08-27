dbfc882c6e7d6228d378a0131af87b869875c83f
Meghal <m3gh4l@gmail.com>
2016-08-27 21:05:01 +0530

    Fixed some bugs for notification

--- app/src/main/java/app/startups/nitrr/ecell/ecellapp/helper/MyFirebaseService.java
+++ app/src/main/java/app/startups/nitrr/ecell/ecellapp/helper/MyFirebaseService.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -110,9 +111,9 @@ public class MyFirebaseService extends FirebaseMessagingService {
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(messageBody))
                 .setAutoCancel(true)
                 .setContentIntent(pendingIntent);
-//        notificationBuilder.setSound(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.sound));
+        notificationBuilder.setSound(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.sound));
 
-        notificationBuilder.setDefaults(Notification.DEFAULT_SOUND|Notification.DEFAULT_LIGHTS|Notification.DEFAULT_VIBRATE);
+        notificationBuilder.setDefaults(Notification.DEFAULT_LIGHTS|Notification.DEFAULT_VIBRATE);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
